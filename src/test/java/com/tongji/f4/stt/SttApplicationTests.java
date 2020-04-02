b126f26fa8519c858b579c04cd7fba3d359257e0
saturnluohao <3099916791@qq.com>
2020-04-02 10:34:39 +0800

    Initial project

---
+++ src/test/java/com/tongji/f4/stt/SttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tongji.f4.stt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SttApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
